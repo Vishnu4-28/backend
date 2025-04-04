85362f9e1a55e1e78d05fc71b07d397eae0c7d91
Vishnu4-28 <vishnuyadav04282003@gmail.com>
2025-04-04 10:37:31 +0530

    update api

--- controllers/userController.js
+++ controllers/userController.js
@@ -60,6 +60,16 @@ exports.verifyEmail = async(req,res) =>{
 
 }
 
+exports.updateUser = async (req,res) =>{
+  try{
+    let user = await User.findByIdAndUpdate(req.params.id,req.body ,{useFindAndModify: false});
+    if(!user) return res.status (404).json({message: "User not found"});
+    res.status(200).json({ message: "User Updated successfully" });
+  }catch(err){
+    res.status(500).json({ message: "server error"});
+  }
+}
+
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -103,6 +113,7 @@ exports.getUsers = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
 
 exports.deleteUser = async (req, res) => {
   try {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { signup, getUsers, deleteUser,login,verifyEmail } = require("../controllers/userController");
+const { signup, getUsers, deleteUser,login,verifyEmail,updateUser} = require("../controllers/userController");
 const router = express.Router();
 const authMiddleware  = require('../middleware/authMiddleware');
 
@@ -7,6 +7,7 @@ router.post("/signup", signup);
 router.get("/verify-Email" ,verifyEmail);
 router.get("/login",login);
 router.get("/getUser",authMiddleware, getUsers);
+router.put("/updateUser/:id",authMiddleware, updateUser);
 router.delete("/delete/:id",authMiddleware, deleteUser);
 
 module.exports = router;
